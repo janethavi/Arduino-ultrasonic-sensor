4f2d942f38f56e1691c9269b9706eef30cdde3ae
Janeth Fernando <janethavishka@gmail.com>
2019-03-08 00:41:43 +0530

    Add files via upload

---
+++ Ultrasonic_sensor.ino
@@ -0,0 +1,33 @@
+// defines pins numbers
+ int trigPin = 9;
+ int echoPin = 8;
+ 
+// defines variables
+long duration;
+int distance;
+
+void setup() {
+pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+Serial.begin(9600); // Starts the serial communication
+}
+void loop() {
+// Clears the trigPin
+digitalWrite(trigPin, LOW);
+// Sets the trigPin on HIGH state for 2 micro seconds
+delayMicroseconds(2);
+
+digitalWrite(trigPin, HIGH);
+// Sets the trigPin on HIGH state for 10 micro seconds
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin, HIGH);
+// Calculating the distance (cm)
+distance= duration*0.034/2;
+
+// Prints the distance on the Serial Monitor
+Serial.print("Distance: ");
+Serial.println(distance);
+}
